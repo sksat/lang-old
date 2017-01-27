644c2412ee54de7e8cba3432befb651df6168dfb
sk2sat <sksat.aesc@gmail.com>
2017-01-27 19:56:41 +0900

    add lang.c

---
+++ lang.c
@@ -0,0 +1,12 @@
+#include "lang.h"
+
+int langMainLoop(FILE *fp){
+	for(;;){
+		if(EXEC_MODE == INTERPRET){
+			printf("> ");
+		}
+		
+	}
+}
+
+
--- main.c
+++ main.c
@@ -6,15 +6,25 @@
 
 int EXEC_MODE;
 
+int langMainLoop(FILE *fp);
+
 int main(int argc, char **argv){
 	FILE *fp;
 	
 	if(argc == 1){
 		EXEC_MODE = INTERPRET;
 		fp = stdin;
+		printf("interpretor mode.\n");
 	}else{
 		EXEC_MODE = COMPILE;
+		fp = fopen(argv[1], "r");
+		if(fp == NULL){
+			printf("error:\n\tcan't open file:%s\n", argv[1]);
+			return -1;
+		}
 	}
+
+	return langMainLoop(fp);
 }
 
 
